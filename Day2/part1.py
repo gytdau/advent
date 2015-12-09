226e0411243aab1f720e5164f70e5dcabd9a74e0
Gytis Daujotas <gytdau@gmail.com>
2015-12-09 06:40:32 +0000

    Modified to take normal input instead of an array

--- Day2/part1.py
+++ Day2/part1.py
@@ -1,10 +1,16 @@
-from inputData import *
+data = []
+
+with open("inputData.txt", "r") as infile:
+    for line in infile:
+        line = line.replace('x', ' ')
+        data += line.split()
+
 
 paper = 0
 for i in range(0, len(data), 3):
-    length = data[i]
-    width = data[i+1]
-    height = data[i+2]
+    length = int(data[i])
+    width = int(data[i+1])
+    height = int(data[i+2])
 
     paper += (2 * length * width) + (2 * width * height) + (2 * height * length)
     paper += min((length * width), (width * height), (height * length))
--- Day2/part2.py
+++ Day2/part2.py
@@ -1,10 +1,15 @@
-from inputData import *
+data = []
+
+with open("inputData.txt", "r") as infile:
+    for line in infile:
+        line = line.replace('x', ' ')
+        data += line.split()
 
 ribbon = 0
 for i in range(0, len(data), 3):
-    length = data[i]
-    width = data[i+1]
-    height = data[i+2]
+    length = int(data[i])
+    width = int(data[i+1])
+    height = int(data[i+2])
     
     things = [length, width, height]
     things.remove(max(things))
