171ac1d8a59bf3910578dc49e04c31f70aa18738
Gytis Daujotas <gytdau@gmail.com>
2015-12-10 18:17:32 +0000

    Added Day 10 - Part 1

---
+++ Day10/part1.py
@@ -0,0 +1,28 @@
+
+def repeat(data):
+    # Quick hack, so sorry
+    data = data + "0"
+
+    iteration = 0
+    result = ""
+
+    while iteration < len(data) - 1:
+        x = iteration + 1
+        character_count = 1
+        char = data[iteration]
+
+        while data[x] == char and x < len(data) - 1:
+            x = min(x + 1, len(data) - 1)
+            character_count += 1
+
+        iteration += character_count
+        result += str(character_count) +str(char)
+    return result
+
+
+actualResult = "1113222113"
+
+for i in range(0, 40):
+    actualResult = repeat(actualResult)
+
+print(len(actualResult))
